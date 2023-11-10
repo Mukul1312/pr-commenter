85df20de73af683ca38736a3fbcc08fd17c41942
Mukul dubey <mdubey.0433@gmail.com>
2023-11-10 05:49:00 +0530

    /explain command for commit message is completed

--- index.js
+++ index.js
@@ -7,38 +7,84 @@ const createPrompt = require("./llm");
 module.exports = triggerPR;
 
 function triggerPR(app) {
-  
-  app.on("pull_request.opened", triggerCommandAndCommentResult);
+  app.on(
+    ["pull_request.opened", "pull_request.edited", "pull_request.synchronize"],
+    triggerCommandAndCommentResult
+  );
 
   async function triggerCommandAndCommentResult(context) {
-    const executableCommand = findCommandFromPullRequestDescription(
+    let commandsArr = [];
+
+    const commandsFromPRDescription = getCommands(
       context.payload.pull_request.body
     );
 
-    if (executableCommand) {
-      const result = await executeCommand(executableCommand, context);
+    const commitMessagesList = await getCommitMessages(context);
+
+    const commandsFromCommitMessages = commitMessagesList.map((commitMessage) =>
+      getCommands(commitMessage)
+    );
+
+    commandsArr = [...commandsFromPRDescription, ...commandsFromCommitMessages]
+      .flat()
+      .filter((command) => command)
+      .reduce((uniqueCommands, command) => {
+        if (!uniqueCommands.includes(command)) {
+          uniqueCommands.push(command);
+        }
+        return uniqueCommands;
+      }, []);
+
+    if (commandsArr.length === 0) {
+      return context.octokit.issues.createComment(
+        context.issue({
+          body: `To execute commands, please add the following commands in the PR description or in the commit messages: \n\n - /execute \n - /explain`,
+        })
+      );
+    }
+
+    commandsArr.forEach(async (command) => {
+      const result = await executeCommand(command, context);
       context.log(result);
       const comment = context.issue({
         body: `${result}`,
       });
       return context.octokit.issues.createComment(comment);
-    }
+    });
   }
 
-  function findCommandFromPullRequestDescription(description) {
+  async function getCommitMessages(context) {
+    const commits = await context.octokit.pulls.listCommits({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      pull_number: context.payload.pull_request.number,
+    });
+
+    // Extract commit messages
+    const commitMessagesList = commits.data.map(
+      (commit) => commit["commit"]["message"]
+    );
+    // context.log(commitMessages);
+
+    return commitMessagesList;
+  }
+
+  function getCommands(description) {
     // Initially We will only support 2 commmands. /execute and /explain
     const executeCommandRegex = /\/execute/i;
     const explainCommandRegex = /\/explain/i;
 
-    if (executeCommandRegex.test(description)) {
-      return "execute";
-    }
+    const commands = [];
 
     if (explainCommandRegex.test(description)) {
-      return "explain";
+      commands.push("explain");
     }
 
-    return null;
+    if (executeCommandRegex.test(description)) {
+      commands.push("execute");
+    }
+
+    return commands;
   }
 
   async function executeCommand(command, context) {
